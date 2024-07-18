32ff1b31f7267c65e3e859994d87a5f7261714c5
smugytea4real <thomasandromeda999@gmail.com>
2024-07-18 15:23:32 +0700

    Database Migration

---
+++ database/migrations/2024_07_18_075912_change_gender_atributes_in_students_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->string('gender', 1)->nullable()->after('nis')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            Schema::table('students', function (Blueprint $table) {
+                $table->string('gender', 10)->after('nis')->change();
+            });
+        });
+    }
+};
