07baef9fdb48e6e95277b1ee275aeca302cdae6f
smugytea4real <thomasandromeda999@gmail.com>
2024-07-19 15:49:25 +0700

    Collection Methods

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -11,53 +11,62 @@ class StudentController extends Controller
 {
     public function index()
     {
-            //     $student = Student::all();
-            //     return view('student', ['studentlist' => $student]);
+        //     $student = Student::all();
+        //     return view('student', ['studentlist' => $student]);
 
-            //     query builder
-            //     $student = DB::table('students')->get();
-            //     dd($student);
-            
-            //     query builder
-            //     $student = DB::table('students')->insert([
-            //     'name' => 'Donny',
-            //     'gender' => 'L',
-            //     'NIS' => '1234567',
-            //     'class_id' => 1
-            //     ]);
+        $nilai = [4,1,2,3,6,5,7,9,8,3,5,3,1,2,6,8,3];
 
-            //     query builder
-            //     $student = DB::table('students')->where('gender', 'P')->get();
-            //     dd($student);
+        // php biasa
+        // $countNilai = count($nilai);
+        // $totalNilai = array_sum($nilai);
+        // $nilaiRataRata = $totalNilai / $countNilai;
 
-            //     query builder
-            //     DB::table('students')->where('id', 32)->update([
-            //     'name' => 'Sonny',
-            //     'class_id' => 2
-            //     ]);
 
-            //     query builder
-            //     DB::table('students')->where('id', 32)->delete();
+        // colection avg
+        // $nilaiRataRata = collect($nilai)->avg();
+        // dd($nilaiRataRata);
 
-            //     eloquent
-            //     $student = Student::all();
-            //     dd($student);
+        // colection diff
+        // $restaurantA = ['martabak', 'bakso', 'soto', 'nasi'];
+        // $restaurantB = ['cendol', 'bakso', 'esteh', 'kari'];
 
-            //     eloquent
-            //     Student::create([
-            //        'name' => 'Denny',
-            //        'gender' => 'P',
-            //        'NIS' => '7654321',
-            //        'class_id' => 2
-            //     ]);
+        // $menuRestoA = collect($restaurantA)->diff($restaurantB);
+        // $menuRestoB = collect($restaurantB)->diff($restaurantA);
 
-            //     eloquent
-            //     Student::find(32)->update([
-            //     'name' => 'Tony',
-            //     'gender' => 'P',
-            //     ]);
+        // dd($menuRestoA);
 
-            //     eloquent
-            //     Student::find(31)->delete();
+        // colection filter
+        // $aaa = collect($nilai)->filter(function ($value) {
+        //     return $value > 4;
+        // })->all();
+    
+        // dd($aaa);
+
+       
+        // $biodata = [
+        //     ['nama' => 'beni', 'umur' => 20],
+        //     ['nama' => 'siti', 'umur' => 21],
+        //     ['nama' => 'buda', 'umur' => 22],
+        // ];
+
+        // $aaa = collect($biodata)->pluck('nama');
+
+        // dd($aaa);
+
+        // mencari tahu hasil dari nilai dikali 2 dari data2 yang ada di array $nilai
+        // $nilaiKaliDua = [];
+        // foreach ($nilai as $value) {
+        //     array_push($nilaiKaliDua, $value * 2);
+        // }
+
+        // dd($nilaiKaliDua);
+
+        // colection pluck
+
+        $aaa = collect($nilai)->map(function ($value) {
+             return $value * 2;
+        });
+
+        dd($aaa);
     }
 }
